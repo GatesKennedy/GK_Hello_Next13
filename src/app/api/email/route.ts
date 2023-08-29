f0822acbc60d96cf30009a2667420df56aab7785
GatesKennedy <34301476+GatesKennedy@users.noreply.github.com>
2023-08-29 11:08:21 -0700

    MOD: Define nodemailer route;;

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,11 +1,63 @@
 import { FormValues } from '@/app/talk/Contact'
+import nodemailer from 'nodemailer'
 
+const transporter = nodemailer.createTransport({
+  pool: true,
+  host: String(process.env.MAIL_HOST),
+  port: Number(process.env.MAIL_PORT),
+  secure: true, // use TLS
+  auth: {
+    user: String(process.env.MAIL_USER),
+    pass: String(process.env.MAIL_PASS),
+  },
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log("Server is ready to take messages");
+	}
+});
 
 
 export async function POST(req:Request) {
-	const body: FormValues = await req.json() as FormValues
+	try {
+		const body: FormValues = await req.json() as FormValues
+		console.log('FormValues > body: ', body)
 
-	console.log('body: ', body)
+		const message = {
+			from: process.env.MAIL_USER,
+			to: ["Conor@GatesKennedy.com", body.email],
+			subject: "Hello from Gates Kennedy",
+			text: `Message: ${body.message}`,
+			html: `<div><h3>Message from '${body.name}': </h3><p>${body.email}</p></div>`
+		};
+		console.log('message: ', message)
 
-	return new Response('Email Sent')
+		transporter.sendMail(message, function(error, info) {
+			console.log('enter sendMail() callback')
+			if (error) {
+				console.log(error);
+			} else {
+				console.log('Email sent: ' + info.response);
+			}
+		});
+
+		return new Response('Email Sent')
+
+	} catch (error: any) {
+		console.log('ERR: ', error)
+		return error
+	}
 }
