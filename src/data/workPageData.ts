567350817f8107dbdcc6a663bb79760c7527560c
GatesKennedy <34301476+GatesKennedy@users.noreply.github.com>
2023-09-05 18:08:54 -0700

    MOD: Work Page - update content;;

--- src/data/workPageData.ts
+++ src/data/workPageData.ts
@@ -8,7 +8,10 @@ import hello_info from '../../public/hello_info-min.png'
 import hello_talk from '../../public/hello_talk.webp'
 import hello_light from '../../public/hello_light.webp'
 import hello_dark from '../../public/hello_dark.webp'
+import hello_talkDemo from '../../public/hello_talk_demo.gif'
 import dentalRisk_demo from '../../public/DentalRisk_demo.gif'
+import dentalRisk_validation from '../../public/dentalRisk_validation.webp'
+import dentalRisk_review from '../../public/dentalRisk_review.webp'
 
 
 export interface ListImage {
@@ -44,7 +47,7 @@ export const projectsData: ProjectProps[] = [
 		description: 'This. A portfolio site of recent work.',
 		images: [
 			{ title: 'Hello', desc: 'Dark Mode', src: hello_dark },
-			{ title: 'Auto-engagement', desc: 'hmu...', src: hello_talk },
+			{ title: 'Auto-engagement', desc: 'hmu...', src: hello_talkDemo },
 			// { title: 'Hello', desc: 'Light Mode', src: hello_light },
 		],
 		attributes: {
@@ -73,7 +76,7 @@ export const projectsData: ProjectProps[] = [
 		attributes: {
 			time: { start: "August '22", end: "June '23" },
 			status: STATUS_TYPE.CLOSED,
-			tags: [TAG.TS, TAG.NEXT, TAG.NODE, TAG.POSTGRES, TAG.CORS, TAG.MUI, TAG.AWS_LAMBDA, TAG.PRISMA, TAG.CHART_JS, TAG.NIVO, TAG.NEXTAUTH  ],
+			tags: [TAG.TS, TAG.NEXT, TAG.NODE, TAG.POSTGRES, TAG.MUI, TAG.AWS_LAMBDA, TAG.PRISMA, TAG.CHART_JS, TAG.NIVO, TAG.NEXTAUTH  ],
 		},
 		features: [
 			{title: 'Data Warehouse', desc:'Data is regularly collected from multiple APIs, processed into easily consumable metrics and stored as historical records'},
@@ -89,8 +92,9 @@ export const projectsData: ProjectProps[] = [
 		description:
 			"New Patient intake form for dentists to collect and provide information about a patient's potential risks related to their dental health.",
 		images: [
-			{ title: 'one', desc: 'about one', src: dentalRisk_demo },
-			{ title: 'two', desc: 'about two', src: info_headShot },
+			{ title: 'Demo', desc: 'Intake process demonstration', src: dentalRisk_demo },
+			{ title: 'Form Validation', desc: 'Each input field has minimum requirements and/or pattern matching for validation.', src: dentalRisk_validation },
+			{ title: 'Review Screen', desc: 'User can confirm no changes need to be made before results are generated and emailed.', src: dentalRisk_review },
 
 		],
 		attributes: {
@@ -99,8 +103,9 @@ export const projectsData: ProjectProps[] = [
 			tags: [TAG.TS, TAG.NEXT, TAG.CHAKRA, TAG.REACTHOOKFORM, TAG.NODEMAILER],
 		},
 		features: [
-			{title: 'Data Warehouse', desc:'Data is regularly collected from multiple APIs, processed into easily consumable metrics and stored as historical records'},
-			{title: 'Dynamic Report Generation', desc:'Custom reports can be built from all available data and exported to a pdf or csv for presentations or further analysis'},
+			{title: 'Automated Emails', desc:'Upon user form submission, the results for the questionnaire are generated and sent to the provided email from the doctor\'s chosen email address.'},
+			{title: 'Form Validation', desc:'Ensure data submitted matches the requirements set forth in the various form controls.'},
+			{title: 'Ease of Use', desc:'Consistency across form and readability are key UX design descision.'},
 		],
 	},
 	{
